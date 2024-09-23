6351545370f3c6f8d90d42bfd583a9633457bef0
llaradearaujo <laradearaujo300@gmail.com>
2024-09-23 19:20:05 +0000

    primeira parte

--- sistema_restaurante.py
+++ sistema_restaurante.py
@@ -1,10 +1,10 @@
 """
 Informe o número da turma: 
-Turma - 
+Turma - G93313
 
 Nome completo dos componentes.
-1 - 
-2 - 
+1 - lara de araujo
+2 - lamony das merces silva
 """
 
 
@@ -12,5 +12,32 @@ import os
 
 # Limpa o terminal.
 os.system("cls || clear") 
+
+print("""
+pratos do dia:
+01- macarrão(R$18)
+02- lasanha(R$25)
+03- batata recheada(R$22)
+04- strogonoff(R$23)
+05- risoto(R$30)
+06- hamburguer(R$26)
+07- camarão á milanesa(R$38)
+
+""")
+
+while codigo_prato != 0:
+    codigo_prato = int(input("digite o código do prato ou 0 para finalizar pedido: "))
+    precos = []
+    match(codigo_prato):
+        case 1:
+            precos.append(18)
+        case 1:
+            precos.append(25)
+        case 1:
+            precos.append(22)
+        case 1:
+            precos.append(18)
+
+
 
 
